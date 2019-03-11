c539ba71776bad8ac598fd61ff36d41dd51f16e4
Ahmed Elfaki <Ahmed.Elfaki@conlance.de>
2019-03-11 13:16:33 +0100

    feat:: prepare for acceptance test

---
+++ app/src/androidTest/java/de/conlance/kotlinclean/AcceptanceTest.kt
@@ -0,0 +1,23 @@
+package de.conlance.kotlinclean
+
+import android.app.Activity
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+abstract class AcceptanceTest<T: Activity>(clazz: Class<T>) {
+
+    @Rule
+    @JvmField
+    val testRule: ActivityTestRule<T> = IntentsTestRule(clazz)
+
+    val checkThat: Matchers = Matchers()
+    val events: Events = Events()
+}
